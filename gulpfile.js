8b135ddd20bc6f26fadc19dcf78481e1a363592c
bahram bakht <bahram249@gmail.com>
2017-06-04 17:34:24 +0430

    add compass for sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -12,6 +13,9 @@ var jsSources = [
   'components/scripts/pixgrid.js',
   'components/scripts/tagline.js',
   'components/scripts/template.js'
+];
+var sassSources = [
+  'components/sass/style.scss'
 ];
 // var End---------------------------------------
 
@@ -27,5 +31,15 @@ gulp.task('js', function() {
     .pipe(concat('script.js'))
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
+});
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+    sass: 'components/sass',
+    images:'builds/development/images',
+    style:'expanded'
+    }))
+      .on('error', gutil.log)
+    .pipe(gulp.dest('builds/development/css'))
 });
 // task End------------------------------------------------
